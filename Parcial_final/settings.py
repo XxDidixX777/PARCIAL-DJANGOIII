5f789d5fcbcd6f5c2a2a9638399fa49c8e0b7249
XxDidixX777 <didiereduardo2006@gmail.com>
2025-11-29 20:15:27 -0500

    Update ALLOWED_HOSTS in settings.py
    Expand allowed hosts for the application.

--- Parcial_final/settings.py
+++ Parcial_final/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-m(==z$x*hvm6=7v0buf5(p3xe&x7^+2bts_5*604+*44q1q13h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['didier.pythonanywhere.com']
+ALLOWED_HOSTS = ['didier.pythonanywhere.com',
+    '*.pythonanywhere.com',
+    'Didier.pythonanywhere.com',  
+    '127.0.0.1',
+    'localhost']
 
 
 # Application definition
